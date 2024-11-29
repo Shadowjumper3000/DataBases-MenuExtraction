dcbd0c67332e72ac8c1edf9c0b4606a8ebe76072
David <96634974+Shadowjumper3000@users.noreply.github.com>
2024-11-29 19:17:28 +0100

    remove old folder

--- Backend/menu/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import upload_pdf, process_text, home
-
-urlpatterns = [
-    path('upload-pdf/', upload_pdf, name='upload_pdf'),
-    path('process-text/', process_text, name='process_text'),
-    path('', home, name='home'),
-]
